16bb655dc8376474b168235caa1fb9b599340a4f
xyjiang <123@123.com>
2017-08-16 15:33:16 +0800

    Submission accepted

--- Contest5/B.cpp
+++ Contest5/B.cpp
@@ -8,18 +8,18 @@ using namespace std;
 const int maxn = 1e5+19;
 
 int T, n;
-int frn[maxn], beh[maxn], sample[maxn], sampleR[maxn], s[maxn], sr[maxn];
+int c[maxn], Lsmaller[maxn], sample[maxn], Rsmaller[maxn], Llarger[maxn], Rlarger[maxn];
 
-void addFrn(int x, int d){
-    while(x <= n){
-        frn[x] += d; x += x&-x;
+void add(int x, int d){
+    while(x < maxn){
+        c[x] += d; x += x&-x;
     }
 }
 
-int sumFrn(int x){
+int sum(int x){
     int ret = 0;
     while(x > 0){
-        ret += frn[x]; x -= x&-x;
+        ret += c[x]; x -= x&-x;
     }
     return ret;
 }
@@ -32,42 +32,27 @@ freopen("test.txt", "r", stdin);
 
     cin >> T;
     while(T--){
-        memset(frn, 0, sizeof(frn));
-        memset(beh, 0, sizeof(beh));
-        memset(s, 0, sizeof(s));
-        memset(sr, 0, sizeof(sr));
         cin >> n;
         for(int i = 0; i < n; i++){
             scanf("%d", &sample[i]);
-            sampleR[n-1-i] = sample[i];
         }
+        memset(c, 0, sizeof(c));
         for(int i = 0; i < n; i++){
-//            if(sumBeh(sampleR[i]) - sumBeh(sampleR[i]-1) != 0){
-//                sr[i] = sumBeh(sampleR[i]) - 1;
-////                addBeh(sampleR[i], 1);
-//            }
-            {
-                sr[i] = sumBeh(sampleR[i]);
-                addBeh(sampleR[i], 1);
-            }
-//            if(sumFrn(sample[i]) - sumFrn(sample[i]-1) != 0){
-//                s[i] = sumFrn(sample[i]) - 1;
-////                addFrn(sample[i], 1);
-//            }
-            {
-                s[i] = sumFrn(sample[i]);
-                addFrn(sample[i], 1);
-            }
+            add(sample[i], 1);
+            Lsmaller[i] = sum(sample[i] - 1);
+            Llarger[i] = sum(100004) - sum(sample[i]);
         }
-        reverse(sr, sr+n);
-        int ans = 0;
-        for(int i = 0; i < n; i++){
-            int numFrn = s[i], numBeh = sr[i];
-            ans += numFrn*(n-1-i-numBeh);
-            ans += (i-numFrn)*numBeh;
-            ans += numFrn * numBeh;
+        memset(c, 0, sizeof(c));
+        int j = 0;
+        for(int i = n - 1; i >= 0; i--, j++){
+            add(sample[i], 1);
+            Rsmaller[i] = sum(sample[i] - 1);
+            Rlarger[i] = sum(100004) - sum(sample[i]);
         }
-        printf("%d\n", ans);
+        long long ans = 0;
+        for(int i = 0; i < n; i++)
+            ans += Lsmaller[i]*Rlarger[i] + Rsmaller[i]*Llarger[i];
+        printf("%lld\n", ans);
     }
 
     return 0;
