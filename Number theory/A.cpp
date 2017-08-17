099b3426390f2cf2ddf660958de02aedca218eb0
xyjiang <656385934@qq.com>
2017-08-17 11:21:28 +0800

    Failed A, while D was accepted

--- Number theory/A.cpp
+++ Number theory/A.cpp
@@ -1,32 +1,79 @@
-#include <iostream>
-#include <cstdio>
-#include <algorithm>
-#include <vector>
-#include <cstring>
-#include <queue>
-using namespace std;
-const int maxn = 504;
-
-int m, n;
-double mat[maxn<<1][maxn];
-
-double gauss(){
-
-}
-
-int main(){
-#ifdef TEST
-freopen("test.txt", "r", stdin);
-#endif // TEST
-
-    while(cin >> n >> m){
-        for(int i = 1; i <= m; i++){
-            for(int j = 1; j <= n+1; j++){
-                scanf("%lf", &mat[i][j]);
-            }
-        }
-
-    }
-
-    return 0;
-}
+//#include <iostream>
+//#include <cstdio>
+//#include <algorithm>
+//#include <vector>
+//#include <cstring>
+//#include <queue>
+//using namespace std;
+//const int maxn = 504;
+//
+//int m, n;
+//bool flag = false, manySolutionFlag = false;
+//double mat[maxn<<1][maxn];
+//
+//int gauss(){
+//    for(int i = 1; i <= n; i++){
+//        bool flag = false;
+//        for(int j = i; j <= m; j++){
+//            if(mat[j][i] != 0){
+//                swap(mat[j], mat[i]);
+//                flag = true;
+//                break;
+//            }
+//        }
+//        if(!flag){
+//            manySolutionFlag = true;
+//            continue;
+//        }
+//        for(int j = i+1; j <= m; j++){
+//            double c = mat[j][i]/mat[i][i];
+//            for(int k = 1; k <= n+1; k++)
+//                mat[j][k] = mat[j][k] - mat[i][k] * c;
+//        }
+//    }
+//    for(int i = 1; i <= m; i++){
+//        int pos = 1;
+//        while(mat[i][pos] == 0 && pos <= n) pos++;
+//        if(pos == n+1 && mat[i][pos+1] != 0)
+//            return 0; // no solution.
+//    }
+//    if(manySolutionFlag)
+//        return -1;    // multiple solutions.
+//    for(int i = n; i >= 1; i--){
+//        for(int j = i - 1; j >= 1; j--){
+//            double c = mat[j][i] / mat[i][i];
+//            for(int k = n+1; k >= 1; k--)
+//                mat[j][k] = mat[j][k] - mat[i][k] * c;
+//        }
+//    }
+//    return 1;         // normal status.
+//}
+//
+//void init(){
+//    flag = manySolutionFlag = false;
+//}
+//
+//int main(){
+//#ifdef TEST
+//freopen("test.txt", "r", stdin);
+//#endif // TEST
+//
+//    while(cin >> n >> m){
+//        init();
+//        for(int i = 1; i <= m; i++){
+//            for(int j = 1; j <= n+1; j++){
+//                scanf("%lf", &mat[i][j]);
+//            }
+//        }
+//        int ans = gauss();
+//        if(ans == 0)
+//            cout << "No solutions" << endl;
+//        else if(ans == -1)
+//            cout << "Many solutions" << endl;
+//        else
+//            for(int i = 1; i <= n; i++)
+//                cout << (long long)mat[i][i] << endl;
+//    }
+//
+//    return 0;
+//}
--- Number theory/D.cpp
+++ Number theory/D.cpp
@@ -1,18 +1,48 @@
-//#include <iostream>
-//#include <cstdio>
-//#include <algorithm>
-//#include <vector>
-//#include <cstring>
-//#include <queue>
-//using namespace std;
-//
-//
-//int main(){
-//#ifdef TEST
-//freopen("test.txt", "r", stdin);
-//#endif // TEST
-//
-//
-//
-//    return 0;
-//}
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <queue>
+using namespace std;
+const int maxn = 1e6+19;
+
+bool isPrime[maxn];
+int primeList[maxn];
+int primeCount, range;
+
+void init(){
+    memset(isPrime, true, sizeof(isPrime));
+    memset(primeList, 0, sizeof(primeList));
+    primeCount = 0;
+}
+
+void eular(){
+    init();
+    for(int i = 2; i <= range; i++){
+        if(isPrime[i]){
+            primeCount++;
+            primeList[primeCount] = i;
+        }
+        for(int j = 1; j <= primeCount; j++){
+            if(i * primeList[j] > range)
+                break;
+            isPrime[i*primeList[j]] = false;
+            if(i % primeList[j] == 0)
+                break;
+        }
+    }
+}
+
+int main(){
+#ifdef TEST
+freopen("test.txt", "r", stdin);
+#endif // TEST
+
+    while(cin >> range){
+        eular();
+        cout << primeCount << endl;
+    }
+
+    return 0;
+}
