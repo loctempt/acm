3a99ca6315612da2f49b499732e53c6ed0325ab0
xyjiang <656385934@qq.com>
2017-08-17 11:42:11 +0800

    some comments added

--- Number theory/D.cpp
+++ Number theory/D.cpp
@@ -17,18 +17,20 @@ void init(){
     primeCount = 0;
 }
 
-void eular(){
+void eular(){ //欧拉筛子
     init();
     for(int i = 2; i <= range; i++){
         if(isPrime[i]){
             primeCount++;
-            primeList[primeCount] = i;
+            primeList[primeCount] = i; //素数表是逐渐加长的
         }
         for(int j = 1; j <= primeCount; j++){
             if(i * primeList[j] > range)
                 break;
-            isPrime[i*primeList[j]] = false;
-            if(i % primeList[j] == 0)
+            isPrime[i*primeList[j]] = false; //将每个数的素数倍标记为合数；这种枚举值乘
+	    				     //上质数的方法确保质数表只增大，之前确定的
+					     //质数不再重复判断并加以重复标记。
+            if(i % primeList[j] == 0) //若当前枚举值已经为合数即中断
                 break;
         }
     }
@@ -46,3 +48,4 @@ freopen("test.txt", "r", stdin);
 
     return 0;
 }
+
