3c9108713cf64f4562d83999dd718d1c8d1e6293
xyjiang <656385934@qq.com>
2017-08-17 16:45:53 +0800

    ready to submmit solution to question B

--- Number theory/A.cpp
+++ Number theory/A.cpp
@@ -6,82 +6,64 @@
 #include <queue>
 using namespace std;
 const int maxn = 504;
+const double PCS = 1e-8;
 
 int m, n;
 bool flag = false, manySolutionFlag = false;
-double mat[maxn<<1][maxn], b[maxn], value[maxn];
+double mat[maxn << 1][maxn], b[maxn], value[maxn];
 
-void dbgDisplay(){
-    for(int i = 1; i <= m; i++){
-        for(int j = 1; j <= n+1; j++)
+void dbgDisplay() {
+    for(int i = 1; i <= m; i++) {
+        for(int j = 1; j <= n; j++)
             printf("%8.4f ", mat[i][j]);
-        putchar('\n');
+        printf("%8.4f\n", b[i]);
     }
     putchar('\n');
 }
 
-int gauss(){
-    for(int i = 1; i <= n; i++){
-        bool flag = false;
-        for(int j = i; j <= m; j++){
-            if(mat[j][i] != 0){
-                swap(mat[j], mat[i]);
-                flag = true;
-                break;
-            }
+int gauss() {
+    for(int i = 1; i <= n; i++) {
+        int r = i;
+        for(int j = i + 1; j <= m; j++) {
+            if(fabs(mat[j][i]) > fabs(mat[r][i]))
+                r = j;
         }
-        if(!flag){
-            manySolutionFlag = true;
-            continue;
-        }
-        for(int j = i+1; j <= m; j++){
-            double c = mat[j][i]/mat[i][i];
-            for(int k = 1; k <= n; k++)
+        if(r == i && fabs(mat[i][i]) < PCS)
+            return -1;                          // multiple solutions.
+        swap(mat[i], mat[r]);
+        for(int j = i + 1; j <= m; j++) {       // 变换为上三角阵
+            double c = mat[j][i] / mat[i][i];
+            for(int k = 1; k <= m; k++) {
                 mat[j][k] -= mat[i][k] * c;
-            b[j] -= b[i]*c;
+            }
+            b[j] -= b[i] * c;
         }
-//        dbgDisplay();
+        dbgDisplay();
     }
-//    cout << "上三角阵已建立\n即将建立下三角阵\n" << endl;
-    for(int i = 1; i <= m; i++){
+    for(int j = 1; j <= m; j++){
         int pos = 1;
-        while(fabs(mat[i][pos]) < 1e-8 && pos <= n) pos++;
-        if(pos == n+1 && fabs(b[i]) > 1e-8)
-            return 0; // no solution.
+        while(pos <= n && fabs(mat[j][pos]) < PCS) pos++;
+        if(pos == n + 1 && fabs(b[j]) > PCS)
+            return 0;                           // no solution.
     }
-    if(manySolutionFlag)
-        return -1;    // multiple solutions.
-//    for(int i = n; i >= 1; i--){
-//        for(int j = i - 1; j >= 1; j--){
-//            double c = mat[j][i] / mat[i][i];
-//            for(int k = n+1; k >= 1; k--)
-//                mat[j][k] = mat[j][k] - mat[i][k] * c;
-//        }
-    for(int i = n; i >= 1; i--){
-        for(int j = i + 1; j <= n; j++){
+    for(int i = n; i >= 1; i--) {
+        for(int j = i + 1; j <= n; j++) {
             b[i] -= mat[i][j] * value[j];
             mat[i][j] = 0;
         }
         value[i] = b[i] / mat[i][i];
     }
-//        dbgDisplay();
-
     return 1;         // normal status.
 }
 
-void init(){
-    flag = manySolutionFlag = false;
-}
-
-int main(){
+int main() {
 #ifdef TEST
-freopen("test.txt", "r", stdin);
+    freopen("test.txt", "r", stdin);
 #endif // TEST
 
-    while(cin >> n >> m){
-        init();
-        for(int i = 1; i <= m; i++){
-            for(int j = 1; j <= n; j++){
+    while(cin >> n >> m) {
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
                 scanf("%lf", &mat[i][j]);
             }
             scanf("%lf", &b[i]);
