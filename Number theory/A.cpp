34580574eb2daf8f5ce57b4de2a6a6b5e099841c
xyjiang <656385934@qq.com>
2017-08-17 10:16:44 +0800

    ready to try pushing

--- Number theory/A.cpp
+++ Number theory/A.cpp
@@ -5,14 +5,28 @@
 #include <cstring>
 #include <queue>
 using namespace std;
+const int maxn = 504;
 
+int m, n;
+double mat[maxn<<1][maxn];
+
+double gauss(){
+
+}
 
 int main(){
 #ifdef TEST
 freopen("test.txt", "r", stdin);
 #endif // TEST
 
+    while(cin >> n >> m){
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n+1; j++){
+                scanf("%lf", &mat[i][j]);
+            }
+        }
 
+    }
 
     return 0;
 }
