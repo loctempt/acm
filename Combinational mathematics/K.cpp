4e7b8f8a077530dd148e655d2f4454a68ae1d685
xyjiang <656385934@qq.com>
2017-08-18 17:52:59 +0800

    Aug 18th. Before leaving check

--- Combinational mathematics/K.cpp
+++ Combinational mathematics/K.cpp
@@ -1,32 +1,34 @@
-#include <iostream>
-#include <cstdio>
-#include <algorithm>
-#include <vector>
-#include <cstring>
-#include <queue>
-#undef TEST
-typedef long long ll;
-using namespace std;
-const int MOD = 1000007;
-
-ll qpow(ll a, ll b){
-    int ret = 1, y = a;
-    while(b){
-        if(b & 1) ret = ret * y % MOD;
-        y = y * y % MOD;
-        b >>= 1;
-    }
-    return ret;
-}
-
-int main(){
-#ifdef TEST
-freopen("test.txt", "r", stdin);
-#endif // TEST
-
-    int k;
-    while(cin >> k)
-        cout << qpow(2, k) << endl;
-
-    return 0;
-}
+//#include <iostream>
+//#include <cstdio>
+//#include <algorithm>
+//#include <vector>
+//#include <cstring>
+//#include <queue>
+//typedef long long ll;
+//using namespace std;
+//const int MOD = 100003;
+//
+//int qpow(ll base, ll exponent) {
+//    int ret = 1;
+//    while(exponent) {
+//        if(exponent & 1) ret = ret * base % MOD;
+//        base = base * base % MOD;
+//        exponent >>= 1;
+//    }
+//    return ret;
+//}
+//
+//int main() {
+//#ifdef TEST
+//    freopen("test.txt", "r", stdin);
+//#endif // TEST
+//
+//    ll m, n;
+//    cin >> m >> n;
+//    ll ans = (qpow(m, n) % MOD - m * qpow(m - 1, n - 1) % MOD) % MOD;
+//    if(ans < 0)
+//        ans += MOD;
+//    cout << ans << endl;
+//
+//    return 0;
+//}
