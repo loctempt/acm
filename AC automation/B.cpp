df9322b7da0ea0b67572422c1a63ee01856cd60a
xyjiang <656385934@qq.com>
2017-08-25 21:37:17 +0800

    solution to question B was accepted

--- AC automation/B.cpp
+++ AC automation/B.cpp
@@ -2,15 +2,18 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-const int maxNode = 500, sigma_size = 26;
+const int maxNode = 4003*104, sigma_size = 26, MOD = 20071027;
 int ch[maxNode][sigma_size];
 int val[maxNode];
+int dp[300004];
+char sample[300004];
 
 struct Trie{
     int sz;
     Trie(){
         sz = 1;
         memset(ch[0], 0, sizeof(ch[0]));
+        memset(dp, 0, sizeof(dp));
     }
     int idx(char c){
         return c - 'a';
@@ -36,23 +39,39 @@ struct Trie{
         }
         return val[u];
     }
+    int hack(){
+        int len = strlen(sample);
+        for(int i = len-1; i >= 0; i--){
+            int u = 0;
+            for(int j = i; j < len; j++){
+                int c = idx(sample[j]);
+                if(!ch[u][c])
+                    break;
+                u = ch[u][c];
+                if(val[u])
+                    dp[i] = (dp[j+1]+dp[i])%MOD;
+            }
+        }
+    }
 };
 
 int main(){
 #ifdef TEST
 freopen("test.txt", "r", stdin);
 #endif
-
-    char str[50], Case = 1;
-    Trie Td;
-    while(~scanf("%s", str)){
+    int Case = 1;
+    char str[104];
+    while(~scanf("%s", sample)){
+        Trie Td;
         int x;
         cin >> x;
-        if(x){
-            Td.insert(str, Case++);
+        for(int i = 1; i <= x; i++){
+            scanf("%s", str);
+            Td.insert(str, 1);
         }
-        else
-            cout << Td.query(str) << endl;
+        dp[strlen(sample)] = 1;
+        Td.hack();
+        printf("Case %d: %d\n", Case++, dp[0]);
     }
 
     return 0;
